1c80436f612ea690e0d3bf159e12445aa6697139
unknown <lucas.bortoliborges@gmail.com>
2023-11-28 19:49:29 -0300

    configurando cors 2

---
+++ src/main/java/com/project/api/core/WebConfig.java
@@ -0,0 +1,17 @@
+package com.project.api.core;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:2000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowCredentials(true);
+    }
+}
