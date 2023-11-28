e5a92f9a2713708ffe601dca6d973eb5474bd8ad
unknown <lucas.bortoliborges@gmail.com>
2023-11-28 19:37:49 -0300

    configurando cors

---
+++ src/main/java/com/project/api/core/CorsConfig.java
@@ -0,0 +1,29 @@
+package com.project.api.core;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+
+        // Allow requests from your React application on S3
+        config.addAllowedOrigin("http://localhost:2000");
+
+        // Allow common HTTP methods (GET, POST, etc.)
+        config.addAllowedMethod("*");
+
+        // Allow headers you want to handle (e.g., Authorization, Content-Type)
+        config.addAllowedHeader("*");
+
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
