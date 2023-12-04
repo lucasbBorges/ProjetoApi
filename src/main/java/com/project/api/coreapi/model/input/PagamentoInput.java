f2b2bc754ce33451a058a5473568ac27a4a1b6e3
Lucas Borges <lucas.bortoliborges@gmail.com>
2023-12-04 09:13:44 -0300

    corrigindo bug data

--- src/main/java/com/project/api/coreapi/model/input/PagamentoInput.java
+++ src/main/java/com/project/api/coreapi/model/input/PagamentoInput.java
@@ -1,9 +1,11 @@
 package com.project.api.coreapi.model.input;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Getter
@@ -12,6 +14,7 @@ public class PagamentoInput {
     private BigDecimal valor;
     private ClienteInput cliente;
     private MeioPagamentoInput meioPagamento;
-    private LocalDateTime dataPagamento;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    private LocalDate dataPagamento;
 
 }
